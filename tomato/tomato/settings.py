1c3ad9dd4dafa9c93a3dddfc6bc2cfca7a7eaf49
Pavithran-Vijayalakshmi <pavithranvijayalakshmi@gmail.com>
2025-07-08 15:44:32 +0530

    Merged with Docker

--- tomato/tomato/settings.py
+++ tomato/tomato/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,16 +85,35 @@ WSGI_APPLICATION = 'tomato.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "FoodDelivery",
+#         "USER": "foodDeliveryUser",
+#         "PASSWORD": "raju",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
+import os
+
+import os
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "FoodDelivery",
-        "USER": "foodDeliveryUser",
-        "PASSWORD": "raju",
-        "HOST": "localhost",
-        "PORT": "5433",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST", "localhost"),
+        'PORT': os.environ.get("DB_PORT", 5432),
     }
 }
+
+
+
+
 
 
 # Password validation
