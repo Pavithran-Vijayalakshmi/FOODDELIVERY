dd92b6220fb640feff43737a03379e99b55d878c
Pavithran-Vijayalakshmi <pavithranvijayalakshmi@gmail.com>
2025-07-09 12:52:28 +0530

    Created apps named restaurants, user and orders

--- tomato/restaurants/urls.py
+++ tomato/restaurants/urls.py
@@ -4,9 +4,6 @@ from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
 router.register(
-    'List of Restaurants', restaurantsList , basename= ' ListOfRestaurants'
-)
-router.register(
     'menu-items', MenuList, basename='menuitems'
 )
 router.register(
@@ -19,4 +16,6 @@ router.register(
     'cart', CartList, basename='cart'
 )
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('ListofRestaurants/', restaurantsList.as_view() ,name = ' List Of Restaurants')
+    ]+router.urls
--- tomato/user/views.py
+++ tomato/user/views.py
@@ -8,9 +8,17 @@ from rest_framework.permissions import AllowAny
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
 class ListUser(ListCreateAPIView):
-    queryset = userClass.objects.all()
+    permission_classes = [AllowAny]
     serializer_class = userSerializer
+
+    def get_queryset(self):
+        queryset = userClass.objects.all()
+        name = self.request.query_params.get('name')
+        if name:
+            queryset = queryset.filter(name=name)  # or use icontains for case-insensitive partial matches
+        return queryset
     
 class DetailedUser(RetrieveUpdateDestroyAPIView):
+    permission_classes = [AllowAny]
     queryset = userClass.objects.all()
     serializer_class = userSerializer
